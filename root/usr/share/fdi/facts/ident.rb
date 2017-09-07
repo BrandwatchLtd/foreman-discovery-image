8720cfa810c137f933c4345f0ebfebce6e63754f
Luke Alexander <luke@brandwatch.com>
2017-09-07 11:26:10 +0100

    adding ipmi serial number fact

--- root/usr/share/fdi/facts/ident.rb
+++ root/usr/share/fdi/facts/ident.rb
@@ -5,8 +5,13 @@
 Facter.add("fdident", :timeout => 10) do
   setcode do
     ident = Facter.fact("serialnumber").value
+    ipmi_serial = Facter.fact("ipmi_fru_board_serial").value
     if ident.match(/12345678/)
-      ident = Facter.value(:discovery_bootif).gsub(/:/,'')
+      if ipmi_serial
+        ident = ipmi_serial
+      else
+        ident = Facter.value(:discovery_bootif).gsub(/:/,'')
+      end
     end
     ident
   end
