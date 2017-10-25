5bf3d014206645da67be86910c808a09409cb739
Luke Alexander <luke@brandwatch.com>
2017-10-25 16:09:35 +0100

    update ipmi fact

--- root/usr/share/fdi/facts/ident.rb
+++ root/usr/share/fdi/facts/ident.rb
@@ -5,13 +5,11 @@
 Facter.add("fdident", :timeout => 10) do
   setcode do
     ident = Facter.fact("serialnumber").value
-    ipmi_serial = Facter.fact("ipmi_fru_board_serial").value
-    if ident.match(/12345678/)
-      if ipmi_serial
-        ident = ipmi_serial
-      else
-        ident = Facter.value(:discovery_bootif).gsub(/:/,'')
-      end
+    ipmi_serial = Facter.fact("ipmi_fru_product_serial").value
+    if ipmi_serial and not ipmi_serial.match(/12345678/)
+      ident = ipmi_serial
+    elsif ident.match(/12345678/)
+      ident = Facter.value(:discovery_bootif).gsub(/:/,'')
     end
     ident
   end
