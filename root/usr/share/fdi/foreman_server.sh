88f7c0e48b623ce82a677e33d6b63334ae0053be
Luke Alexander <luke@brandwatch.com>
2018-09-17 14:20:16 +0100

    fix foreman facts

--- root/usr/share/fdi/foreman_server.sh
+++ root/usr/share/fdi/foreman_server.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 foreman_ip=$( facter dhcp_servers | cut -d '"' -f 4 )
+until [[ "$foreman_ip" =~ ^10 ]]; do
+  echo "Foreman ip fact not yet set - sleeping!"
+  sleep 2
+done
 foreman_server=$( facter foreman_server )
-if [[ "$foreman_ip" =~ ^10 ]]; then
-  echo -e "${foreman_ip}\t${foreman_server}" >> /etc/hosts
-fi
+until [[ "$foreman_server" =~ ^theforeman ]]; do
+  echo "Foreman server fact not yet set - sleeping!"
+  sleep 2
+done
+echo -e "${foreman_ip}\t${foreman_server}" >> /etc/hosts
