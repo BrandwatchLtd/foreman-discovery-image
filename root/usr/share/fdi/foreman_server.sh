81f31788caf6890afa1a8af91c03e7d45e3702dc
Luke Alexander <luke@brandwatch.com>
2018-09-18 10:28:03 +0100

    fix hard code address

--- root/usr/share/fdi/foreman_server.sh
+++ root/usr/share/fdi/foreman_server.sh
@@ -3,3 +3,4 @@ foreman_ip=$( facter dhcp_servers | cut -d '"' -f 4 )
 sleep 2
 foreman_server=$( facter foreman_server )
 echo -e "${foreman_ip}\t${foreman_server}" >> /etc/hosts
+echo "10.3.104.2\ttheforeman-hay0.brandwatch.net" >> /etc/hosts
