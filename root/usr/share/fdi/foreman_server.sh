1eafeb113aeb28b2f57799b8415ac3d37c83bf3a
Luke Alexander <luke@brandwatch.com>
2019-01-28 12:06:14 +0000

    fix foreman server hosts entry

--- root/usr/share/fdi/foreman_server.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-foreman_ip=$( facter dhcp_servers | cut -d '"' -f 4 )
-sleep 2
-foreman_server=$( facter foreman_server )
-echo "${foreman_ip} ${foreman_server}" >> /etc/hosts
-echo "10.3.104.2 theforeman-hay0.brandwatch.net" >> /etc/hosts
